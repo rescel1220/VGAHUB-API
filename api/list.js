da506f03d29868fd8983b2e500a58719a28b1924
rescel1220 <rezmonllic@gmail.com>
2026-09-11 11:22:16 +0700

    Update list.js

--- api/list.js
+++ api/list.js
@@ -3,20 +3,11 @@ export default async function handler(req, res) {
     // =====================================================
     // CORS
     // =====================================================
-    res.setHeader(
-        "Access-Control-Allow-Origin",
-        "*"
-    );
+    res.setHeader( "Access-Control-Allow-Origin", "*");
 
-    res.setHeader(
-        "Access-Control-Allow-Methods",
-        "GET, OPTIONS"
-    );
-
-    res.setHeader(
-        "Access-Control-Allow-Headers",
-        "Content-Type"
-    );
+    res.setHeader( "Access-Control-Allow-Methods","GET, OPTIONS" );
+    
+    res.setHeader( "Access-Control-Allow-Headers","Content-Type");
     // =====================================================
     // HANDLE OPTIONS
     // =====================================================
