ef475fff11865c44653315774ee3853fba49b9a7
rescel1220 <rezmonllic@gmail.com>
2026-09-14 09:03:58 +0700

    Remove unnecessary code block comments

--- api/upload.js
+++ api/upload.js
@@ -1,4 +1,4 @@
-```javascript
+
 export default async function handler(req, res) {
 
     // =====================================================
@@ -406,4 +406,4 @@ export default async function handler(req, res) {
     }
 
 }
-```
+
