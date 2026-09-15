b9b4d0daf45390e256a8918bbebd8adc2f93b81c
rescel1220 <rezmonllic@gmail.com>
2026-09-15 09:02:57 +0700

    Update list.js

--- api/list.js
+++ api/list.js
@@ -1,4 +1,4 @@
-```javascript
+
 export default async function handler(req, res) {
 
     // =====================================================
@@ -419,4 +419,4 @@ export default async function handler(req, res) {
     }
 
 }
-```
+
